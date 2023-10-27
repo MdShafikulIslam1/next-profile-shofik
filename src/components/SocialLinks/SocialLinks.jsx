e628f0b993916d7a27a0504dd7b8227bff40c770
shafikul-islam <179398ssshofik@gmail.com>
2023-10-27 10:06:02 +0600

    added social link facebook,linkedin,github

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -5,18 +5,18 @@ const SocialLinks = () => {
     return (
         <ul className='text-md flex  gap-x-4  '>
             <li className='p-2 bg-primary text-white rounded-md hover:scale-125 duration-200'>
-                <Link to="https://www.facebook.com/profile.php?id=100088009747582" target='_blank'>
+                <Link to="https://www.facebook.com/profile.php?id=100013695768823" target='_blank'>
                     <FaFacebookF />
                 </Link>
             </li>
             <li className='p-2 bg-primary text-white rounded-md hover:scale-125 duration-200'>
-                <Link to="https://www.linkedin.com/in/md-habibur-rahman4542" target='_blank'>
+                <Link to="https://www.linkedin.com/in/md-shofikul-islam-shofik-0835a5259" target='_blank'>
                     <FaLinkedinIn />
                 </Link>
 
             </li>
             <li className='p-2 bg-primary text-white rounded-md hover:scale-125 duration-200'>
-                <Link to="https://github.com/habibur-pro" target='_blank'>
+                <Link to="https://github.com/" target='_blank'>
                     <FaGithub />
                 </Link>
             </li>
